c92a3865246b0f58cea972343242dac9aa71725c
Deep Patel <155991468+deep3119@users.noreply.github.com>
2025-01-19 22:53:17 +0530

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Main from "./Components/Main"
+import Navbar from "./Components/Navbar"
+
+function App() {
+
+  return (
+    <div className="__className_ea5f4b antialiased min-h-screen bg-gradient-to-b from-neutral-900 to-black text-neutral-400">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
